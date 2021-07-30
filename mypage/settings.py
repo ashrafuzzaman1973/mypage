668aff964876e912c8bebb2d39ef4abf5b8c09e8
ashrafuzzaman1973 <ashrafdiu1973@gmail.com>
2021-07-30 20:28:34 +0600

    new blade add

--- mypage/settings.py
+++ mypage/settings.py
@@ -123,4 +123,8 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
