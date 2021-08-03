8f82d0928aa7d336244861ada19695f70c1cda33
ashrafuzzaman1973 <ashrafdiu1973@gmail.com>
2021-08-04 01:39:13 +0600

    travello destination migration create

--- travello/models.py
+++ travello/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Destination:
-    id:int
-    name:str
-    image:str
-    desc : str
-    price: int
+class Destination(models.Model):
+    name=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='pics')
+    desc =models.TextField()
+    price=models.IntegerField
+    offer=models.BooleanField(default=False)
--- travello/views.py
+++ travello/views.py
@@ -9,18 +9,21 @@ def index(request):
     des1.desc = 'This city never sleep'
     des1.image = 'destination_1.jpg'
     des1.price = 700
+    des1.offer = False
 
     des2 = Destination()
     des2.name = 'Chittagong'
     des2.desc = 'This city of beach'
     des2.image = 'destination_2.jpg'
     des2.price = 750
+    des2.offer = True
 
     des3 = Destination()
     des3.name = 'Rangpur'
     des3.desc = 'This city of silent'
     des3.image = 'destination_3.jpg'
     des3.price = 650
+    des3.offer = False
 
     dests = [des1,des2,des3]
     return render(request,'index.html',{'dests':dests})
